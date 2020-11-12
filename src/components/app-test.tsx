a14fefb0a29085a80a037db9f6148decb794aec3
sfali <syed.ali@yoppworks.com>
2020-11-12 08:57:08 -0500

    updated test

--- src/components/app-test.tsx
+++ src/components/app-test.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react';
 import ArabicButton from './arabic-button';
+import ToggleSelecter from './toggle-selecter';
 import { ArabicLetter } from './model/arabic-letter';
+import Emitter from '../services/event-emitter';
 
 export default class AppTest extends React.Component {
+
+    componentDidMount() {
+        Emitter.on('toggle-state-changed', (newValue) => console.log(newValue))
+    }
+
+    componentWillUnmount() {
+        Emitter.off('toggle-state-changed');
+    }
 
     render() {
         return (
@@ -10,6 +20,11 @@ export default class AppTest extends React.Component {
                 <div>
                     <span className="arabicTitle">Arabic Buttons</span>
                     <div><ArabicButton letter={ArabicLetter.LAM} />&nbsp;<ArabicButton letter={ArabicLetter.AIN} />&nbsp;<ArabicButton letter={ArabicLetter.FA} /></div>
+                </div>
+                <div>&nbsp;</div>
+                <div>
+                    <span className="arabicTitle">Toggle selecter</span>
+                    <div><ToggleSelecter value={ArabicLetter.MEEM} className="arabicButton ui-button p-button-raised"/></div>
                 </div>
             </div>
         );
