3ea3ad763404b05a3e5f99fd313bd36e058e5784
sfali23 <f.syed.ali@gmail.com>
2020-11-17 10:58:03 -0500

    updated

--- src/components/app-test.tsx
+++ src/components/app-test.tsx
@@ -1,16 +1,33 @@
-import * as React from 'react';
-import ArabicKeyboard from './arabic-keyboard';
-import { ArabicLetter } from './model/arabic-letter';
 // import Emitter from '../services/event-emitter';
 import { Button } from 'primereact/button';
-import { RootLetters } from './model/root-letters';
+import * as React from 'react';
 import InputTable from './input-table';
+import { ArabicLetter } from './model/arabic-letter';
 import { InputData } from './model/models';
 import { NamedTemplate } from './model/named-template';
+import { RootLetters } from './model/root-letters';
+import { VerbalNoun } from './model/verbal-noun';
+import VerbalNounPicker from './verbal-noun-picker';
 
-export default class AppTest extends React.Component {
+interface Props { }
 
-    keyboardRef: any = React.createRef();
+interface State {
+    initialValues: VerbalNoun[]
+    showVerbalNounPicker: boolean
+}
+
+export default class AppTest extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+
+        this.updateState = this.updateState.bind(this);
+
+        this.state = {
+            initialValues: [VerbalNoun.VERBAL_NOUN_FORM_II],
+            showVerbalNounPicker: false
+        }
+    }
 
     componentDidMount() {
         //Emitter.on('button-clicked', (value: ArabicLabel) => console.log(`Button clicked: ${value.name}`));
@@ -22,16 +39,22 @@ export default class AppTest extends React.Component {
         // Emitter.off('letters-selected');
     }
 
+    private updateState(selectedValues: VerbalNoun[]) {
+        this.setState({
+            initialValues: selectedValues,
+            showVerbalNounPicker: false
+        });
+    }
+
     render() {
-        const rootLetters: RootLetters = new RootLetters(ArabicLetter.SEEN, ArabicLetter.LAM, ArabicLetter.MEEM)
-        const data = [new InputData(rootLetters, NamedTemplate.FORM_IV_TEMPLATE), new InputData()]
+        const rootLetters: RootLetters = new RootLetters(ArabicLetter.FA, ArabicLetter.TA, ArabicLetter.HHA)
+        const data = [new InputData(rootLetters, NamedTemplate.FORM_I_CATEGORY_A_GROUP_A_TEMPLATE), new InputData()]
         return (
             <div>
                 <div>
-
-                    <ArabicKeyboard ref={(el) => this.keyboardRef = el} initialLetters={rootLetters} onHide={(e) => console.log(`>>>>: ${JSON.stringify(e)}`)} />
-                    <span className="arabicTitle">Keyboard</span>
-                    <div><Button label="Show keyboard" onClick={(e) => this.keyboardRef.show()} aria-haspopup aria-controls="overlay_panel" /></div>
+                    <VerbalNounPicker initalValues={this.state.initialValues} showDialog={this.state.showVerbalNounPicker} onHide={this.updateState} />
+                    <span className="arabicTitle">Verbal Nouns</span>
+                    <div><Button label="Show VerbalNouns" onClick={() => this.setState({ showVerbalNounPicker: true })} aria-haspopup aria-controls="overlay_panel" /></div>
                 </div>
                 <div>&nbsp;</div>
                 <div>
