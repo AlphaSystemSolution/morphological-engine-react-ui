d556df93921df10348469ffd869d985252f06ccb
sfali23 <f.syed.ali@gmail.com>
2020-11-17 10:55:04 -0500

    updated equals function

--- src/components/model/arabic-label.tsx
+++ src/components/model/arabic-label.tsx
@@ -1,7 +1,7 @@
 export class ArabicLabel {
     constructor(public name: string, public label: string, public code: string) { }
 
-    equals(other: ArabicLabel): boolean {
-        return (other !== null) && (other.name === this.name);
+    equals(other?: ArabicLabel): boolean {
+        return other ? other.code === this.code : false;
     }
 }
