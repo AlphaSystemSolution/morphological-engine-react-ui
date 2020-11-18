3fb4124049eca47eff621a0f2ab40b68975e92ac
sfali23 <f.syed.ali@gmail.com>
2020-11-18 16:33:30 -0500

    import project

--- src/components/project-view.tsx
+++ src/components/project-view.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 
+import * as path from 'path';
 import { TabView, TabPanel } from 'primereact/tabview';
-import { Project } from './model/models';
+import { InputData, Project } from './model/models';
 import InputTable from './input-table';
 import Emitter from '../services/event-emitter';
 import { AppToolbar } from '../AppToolbar';
+import { RootLetters } from './model/root-letters';
+import { ConjugationTemplate } from "../components/model/conjugation-template";
+import { ApplicationController } from '../services/application-controller';
+import { ArabicLetter } from './model/arabic-letter';
+import { NamedTemplate } from './model/named-template';
+import { VerbalNoun } from './model/verbal-noun';
 
 interface Props { }
 
@@ -15,9 +22,12 @@ interface State {
 
 export class ProjectView extends React.Component<Props, State> {
 
+    private applicationController: ApplicationController
+
     constructor(props: Props) {
         super(props);
 
+        this.applicationController = new ApplicationController();
         this.state = {
             activeTabIndex: 0,
             projects: [new Project("Untitled1", "Untitled1.json")]
@@ -43,8 +53,43 @@ export class ProjectView extends React.Component<Props, State> {
         });
     }
 
-    private importProject(file: any) {
-        console.log(`>>>>> ${file}`)
+    private importProject(file: File) {
+        const fileReader = new FileReader();
+        fileReader.readAsText(file);
+        fileReader.onload = () => {
+            const content = fileReader.result as string;
+            const ct: ConjugationTemplate = Object.assign(new ConjugationTemplate(), JSON.parse(content));
+            let data: InputData[] = [];
+            ct.data.forEach((item) => {
+                const rootLetters = item.rootLetters
+                data.push(new InputData(
+                    new RootLetters(
+                        ArabicLetter.getByName(rootLetters.firstRadical),
+                        ArabicLetter.getByName(rootLetters.secondRadical),
+                        ArabicLetter.getByName(rootLetters.thirdRadical),
+                        !rootLetters.fourthRadical ? ArabicLetter.TATWEEL : ArabicLetter.getByName(rootLetters.fourthRadical)
+                    ),
+                    NamedTemplate.getByName(item.template),
+                    item.translation,
+                    item.configuration.removePassiveLine,
+                    item.configuration.skipRuleProcessing,
+                    item.verbalNouns.map((name) => VerbalNoun.getByName(name)),
+                    item.id
+                ));
+            });
+
+            const project = new Project(path.basename(file.name, ".json"), file.name, data, ct.chartConfiguration)
+            const projects = [...this.state.projects];
+            projects.push(project);
+            this.setState({
+                projects: projects,
+                activeTabIndex: projects.length - 1
+            });
+        };
+        fileReader.onerror = () => {
+            console.error(`Error reading file: ${file.name}`)
+            throw new Error("Unable to read file");
+        }
     }
 
     render() {
