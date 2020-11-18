4f8d09668d5ff0a985e6cbcf6ec39c59a4eedcd7
sfali23 <f.syed.ali@gmail.com>
2020-11-17 22:19:55 -0500

    create new project

--- src/AppToolbar.tsx
+++ src/AppToolbar.tsx
@@ -3,15 +3,31 @@ import * as React from 'react';
 import { Toolbar } from 'primereact/toolbar';
 import { Button } from 'primereact/button';
 // import { SplitButton } from 'primereact/splitbutton';
+import Emitter from './services/event-emitter';
 
-export default class AppToolbar extends React.Component {
+interface Props { }
+
+interface State { }
+
+export default class AppToolbar extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+
+        this.createNewProject = this.createNewProject.bind(this);
+
+        this.state = {};
+    }
+
+    private createNewProject() {
+        Emitter.emit('create-new-project', {})
+    }
 
     render() {
         const leftContents: any = (
             <React.Fragment>
-                <Button label="New" icon="pi pi-plus" className="p-mr-2"/>
+                <Button label="New" icon="pi pi-plus" className="p-mr-2" onClick={this.createNewProject} />
                 <span>&nbsp;</span>
-                <Button label="Open" icon="pi pi-file-o" className="p-mr-2"/>
+                <Button label="Open" icon="pi pi-file-o" className="p-mr-2" />
                 <i className="pi pi-bars p-toolbar-separator p-mr-2" />
             </React.Fragment>
         );
--- src/components/project-view.tsx
+++ src/components/project-view.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { TabView, TabPanel } from 'primereact/tabview';
 import { Project } from './model/models';
 import InputTable from './input-table';
+import Emitter from '../services/event-emitter';
 
 interface Props { }
 
@@ -16,10 +17,29 @@ export class ProjectView extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
 
+        this.createNewProject = this.createNewProject.bind(this);
+
         this.state = {
             activeTabIndex: 0,
             projects: [new Project("Untitled1", "Untitled1.json")]
         }
+    }
+
+    componentDidMount() {
+        Emitter.on('create-new-project', () => this.createNewProject());
+    }
+
+    componentWillUnmount() {
+        Emitter.off('create-new-project')
+    }
+
+    private createNewProject() {
+        const p = this.state.projects.filter((project) => project.projectName.startsWith('Untitled'));
+        const projectName = `Untitled${p.length + 1}`;
+        this.state.projects.push(new Project(projectName, `${projectName}.json`))
+        this.setState({
+            projects: this.state.projects
+        });
     }
 
     render() {
