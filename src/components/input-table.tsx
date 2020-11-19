17e366af3e777723435d81cd5cd7d5c81d9fe8ff
sfali23 <f.syed.ali@gmail.com>
2020-11-19 09:35:44 -0500

    view dictionary

--- src/components/input-table.tsx
+++ src/components/input-table.tsx
@@ -8,6 +8,7 @@ import MorphologicalInputForm from './morphological-input-form'
 import { IdGenerator } from '../utils/id-generator';
 import { Dialog } from 'primereact/dialog';
 import { Toolbar } from 'primereact/toolbar';
+import { ArabicLetter } from './model/arabic-letter';
 
 interface Props {
     initialData?: InputData[]
@@ -42,6 +43,7 @@ export default class InputTable extends React.Component<Props, State> {
         this.hideDeleteRowDialog = this.hideDeleteRowDialog.bind(this);
         this.deleteSelectedRows = this.deleteSelectedRows.bind(this);
         this.hideDeleteRowsDialog = this.hideDeleteRowsDialog.bind(this);
+        this.viewDictionary = this.viewDictionary.bind(this);
 
         this.state = {
             data: this.props.initialData ? this.props.initialData : [],
@@ -101,6 +103,8 @@ export default class InputTable extends React.Component<Props, State> {
                 <Button type="button" icon="pi pi-pencil" className="p-button-rounded p-button-success" tooltip="Edit" onClick={() => this.editRow(rowData)} />&nbsp;
                 <Button type="button" icon="pi pi-copy" className="p-button-rounded p-button-success" tooltip="Duplicate" onClick={() => this.duplicateRow(rowData)} />&nbsp;
                 <Button type="button" icon="pi pi-trash" className="p-button-rounded p-button-warning" tooltip="Delete" onClick={() => this.confirmDeleteRow(rowData)} />
+                <br />
+                <Button type="button" icon="pi pi-info" className="p-button-rounded p-button-secondary" tooltip="Dictionary" onClick={() => this.viewDictionary(rowData)} />
             </React.Fragment>
         );
     }
@@ -192,6 +196,24 @@ export default class InputTable extends React.Component<Props, State> {
         this.setState({
             showDeleteRowsDialog: false
         });
+    }
+
+    private viewDictionary(rowData: InputData) {
+        const rootLetters = rowData.rootLetters;
+        let searchString = this.getLetterCode(rootLetters.firstRadical) + this.getLetterCode(rootLetters.secondRadical);
+        if (!rootLetters.secondRadical.equals(rootLetters.thirdRadical)) {
+            searchString += this.getLetterCode(rootLetters.thirdRadical);
+        }
+        const fourthRadical = rootLetters.fourthRadical;
+        if (fourthRadical && !ArabicLetter.TATWEEL.equals(fourthRadical)) {
+            searchString += this.getLetterCode(fourthRadical);
+        }
+        const url = process.env.REACT_APP_DICTIONARY_URL + searchString;
+        window.open(url, '_dictionary');
+    }
+
+    private getLetterCode(letter: ArabicLetter): string {
+        return ArabicLetter.HAMZA.equals(letter) ? ArabicLetter.ALIF.code : letter.code;
     }
 
     private findIndexById(id: string): number {
