ff53bdbd898ca07d63ef1e0f19f1ffcfb5cc11d0
sfali23 <f.syed.ali@gmail.com>
2020-11-23 14:12:04 -0500

    added abbreviated conjugation

--- src/components/morphological-chart-view/morphological-charts-view.tsx
+++ src/components/morphological-chart-view/morphological-charts-view.tsx
@@ -3,17 +3,22 @@ import { Accordion, AccordionTab } from 'primereact/accordion'
 import { MorphologicalChart } from '../model/morphological-chart';
 import { ToggleSelecter } from '../toggle-selecter';
 import { Utils } from '../../utils/utils';
+import { AbbreviatedConjugationView } from './abbreviated-conjugation-view';
+import { AbbreviatedConjugation } from '../model/abbreviated-conjugation';
 
 interface Props {
     charts: MorphologicalChart[]
 }
 
 interface State {
-    activeIndex: number
-    selectedIndex: number
+    activeTabIndex: number
+    selectedChartIndex: number
+    abbreviatedConjugation?: AbbreviatedConjugation
 }
 
 export class MorphologicalChartsView extends React.Component<Props, State> {
+
+    private static NUM_OF_COLUMNS = 8;
 
     constructor(props: Props) {
         super(props);
@@ -21,29 +26,33 @@ export class MorphologicalChartsView extends React.Component<Props, State> {
         this.onChartSelected = this.onChartSelected.bind(this);
 
         this.state = {
-            activeIndex: 0,
-            selectedIndex: 0
+            activeTabIndex: 0,
+            selectedChartIndex: 0
         };
     }
 
     private onChartSelected(payload: any) {
-        console.info(JSON.stringify(payload));
+        const index = payload.index;
+        console.log(`CurrentIndex: ${index}`)
         this.setState({
-            selectedIndex: payload.index
+            selectedChartIndex: index,
+            abbreviatedConjugation: this.props.charts[index].abbreviatedConjugation,
+            activeTabIndex: 1
         });
     }
 
+
     private renderChartTabs() {
         const charts = this.props.charts.map((c) => c.copy());
-        const labels = Utils.chunkArray(charts.map((chart) => chart.label), 8);
+        const labels = Utils.chunkArray(charts.map((chart) => chart.label), MorphologicalChartsView.NUM_OF_COLUMNS);
         const elements =
             labels.map((labelArray, parentIndex) => {
                 const labelElements = labelArray.map((label, index) => {
-                    const key = (parentIndex * 10) + index;
+                    const key = (parentIndex * MorphologicalChartsView.NUM_OF_COLUMNS) + index;
                     return (
                         <td key={"chart-label-row" + key}>
                             <ToggleSelecter value={label} index={key} className="chartSelector ui-button p-button-raised"
-                                userKey={label.id} checked={this.state.selectedIndex === key} onChange={this.onChartSelected} />&nbsp;
+                                userKey={label.id} checked={this.state.selectedChartIndex === key} onChange={this.onChartSelected} />&nbsp;
                         </td>
                     );
                 });
@@ -55,10 +64,14 @@ export class MorphologicalChartsView extends React.Component<Props, State> {
     }
 
     render() {
+        const abbreviatedConjugation = this.state.abbreviatedConjugation ? this.state.abbreviatedConjugation : this.props.charts[0].abbreviatedConjugation;
         return (
             <React.Fragment>
-                <Accordion activeIndex={this.state.activeIndex} onTabChange={(e) => this.setState({ activeIndex: e.index })}>
+                <Accordion activeIndex={this.state.activeTabIndex} onTabChange={(e) => this.setState({ activeTabIndex: e.index })}>
                     <AccordionTab header="Contents">{this.renderChartTabs()}</AccordionTab>
+                    <AccordionTab header="Conjugation">
+                        <AbbreviatedConjugationView conjugation={abbreviatedConjugation} />
+                    </AccordionTab>
                 </Accordion>
             </React.Fragment>
         );
