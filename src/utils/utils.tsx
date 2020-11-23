cdf510ac38dfa2fbf093887971668c9a5208f903
sfali23 <f.syed.ali@gmail.com>
2020-11-23 10:58:02 -0500

    functin to chunk array

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -3,4 +3,12 @@ export class Utils {
     public static copyArray(array: any[]) {
         return array.map(x => Object.assign({}, x));
     }
+
+    public static chunkArray<T>(srcArray: T[], chunkSize: number): T[][] {
+        const result = [];
+        while (srcArray.length) {
+            result.push(srcArray.splice(0, chunkSize));
+        }
+        return result;
+    }
 }
