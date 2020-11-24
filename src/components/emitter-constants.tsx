90673b6e05c2111b29341de68d61b6a4950e5de4
sfali23 <f.syed.ali@gmail.com>
2020-11-24 12:25:17 -0500

    new events

--- src/components/emitter-constants.tsx
+++ src/components/emitter-constants.tsx
@@ -1,4 +1,7 @@
 export class EmitterConstants {
     static NEW_PROJECT_ACTION: string = "create-new-project";
     static IMPORT_PROJECT_ACTION: string = "import-project";
+    static ROW_ADDED = "row-added";
+    static ROW_UPDATED = "row-updated";
+    static ROW_DELETED = "row-deleted";
 }
