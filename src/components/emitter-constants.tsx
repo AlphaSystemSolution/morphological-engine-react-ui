ca55d50cc6a105420c22ac479ca97694d091c894
sfali23 <f.syed.ali@gmail.com>
2020-11-30 10:27:03 -0500

    clean up

--- src/components/emitter-constants.tsx
+++ src/components/emitter-constants.tsx
@@ -1,5 +1,4 @@
 export class EmitterConstants {
-    static IMPORT_PROJECT_ACTION: string = "import-project";
     static PROJECT_IMPORTED = "project-imported";
     static PROJECT_SAVED = "save-project";
 }
