6fc66d6b6cafcb72dc81e9fe07f076233361e251
sfali23 <f.syed.ali@gmail.com>
2020-11-30 18:22:52 -0500

    environment specific builds

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+rm -r build
+
+npm run build:docker
+
+docker images -a | grep "morphological-engine-ui" | awk '{print $3}' | xargs docker rmi
+
 docker build -t sfali23/morphological-engine-ui .
+
+docker push sfali23/morphological-engine-ui
